147786e58a85228ad8bb3b3b926eb6387b3197b3
Genesis Leon <Genesis Leon>
2016-05-12 22:04:10 +0200

    falta todavia

--- js/main.js
+++ js/main.js
@@ -1,18 +1,23 @@
 $(function (){
 
 var contador=0;
-var array = new Array(10);
+var array = new Array(9);
 
-$(document).on("click", ".casillero", function(){
+$('#board').find('td').on("click", function(){
 contador++;
 
 if (contador%2 ==0){
-	array.push($(this).html('x'));
+	$(this).html('x');
+	comparar('x');
+	$(this).attr('disabled', true);
+		
 }
 
 else{
-	array.push($(this).html('o'));
-    
+	$(this).html('o');
+	comparar('o');
+	$(this).attr('disabled', true);
+
 }
 
 comparar();
@@ -22,11 +27,84 @@ comparar();
 
 });
 
+function comparar(jugador){
 
-function comparar(){
+if ($('#board').find('#1').text() !== '+'){
+            if ($('#board').find('#1').text() == $('#board').find('#2').text()) {
+                if ($('#board').find('#1').text() == $('#3').text()) {
+                    alert('Win ' + jugador);
+                    $('td').attr('disabled', true);
 
-if (array[0] && array[1] && array[2] == "x"){
-	alert("gano jugador 1")
+               }
+            }
+        //left column check
+            if ($('#board').find('#1').text() == $('#board').find('#4').text()) {
+                if ($('#board').find('#1').text() == $('#7').text()) {
+                    alert('win ' + jugador);
+                    $('td').attr('disabled', true);
+                }
+            }
+        //left diagonal check
+            if ($('#board').find('#1').text() == $('#board').find('#5').text()) {
+                if ($('#board').find('#1').text() == $('#9').text()) {
+                    alert('Win ' + jugador);
+                    $('td').attr('disabled', true);
+                }
+            }    
+        }
+        
+       //middle column check
+        if ($('#board').find('#2').text() !== '+'){
+            if ($('#board').find('#2').text() == $('#board').find('#5').text()) {
+                if ($('#board').find('#2').text() == $('#8').text()) {
+                    alert('win ' + jugador);
+                    $('td').attr('disabled', true);
+                }
+            }
+        }
+        
+        //right column check
+        if ($('#board').find('#3').text() !== '+'){
+            if ($('#board').find('#3').text() == $('#board').find('#6').text()) {
+                if ($('#board').find('#3').text() == $('#9').text()) {
+                    alert('win ' + jugador);
+                    $('td').attr('disabled', true);
+                }
+            }
+            //right diag check
+            if ($('#board').find('#3').text() == $('#board').find('#5').text()) {
+                if ($('#board').find('#3').text() == $('#7').text()) {
+                    alert('win ' + jugador);
+                    $('td').attr('disabled', true);
+                }
+            }
+        }
+        
+        //middle row check
+        
+        if ($('#board').find('#4').text() !== '+'){
+            if ($('#board').find('#4').text() == $('#board').find('#5').text()) {
+                if ($('#board').find('#4').text() == $('#6').text()) {
+                    alert('win ' + jugador);
+                    $('td').attr('disabled', true);
+                }
+            }
+        }
+        
+        //bottom row check
+        if ($('#board').find('#7').text() !== '+'){
+            if ($('#board').find('#7').text() == $('#board').find('#8').text()) {
+                if ($('#board').find('#7').text() == $('#9').text()) {
+                    alert('win ' + jugador);
+                    $('td').attr('disabled', true);
+                }
+            }
+        }
+
+
 }
+  
+$('.game').on('click',function(){
+        $('td').text('+');
+    });
 
-}
